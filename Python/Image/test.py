c6272c90c26f780dc7a7fa2b6ec1babf4beafca3
DK-Lite <kdk5go@gmail.com>
2018-06-04 22:26:53 +0900

    mnist and tensorflow Test

--- Python/Image/test.py
+++ Python/Image/test.py
@@ -1,3 +1,4 @@
+# mnist 실습
 import numpy as np
 import tensorflow as tf
 import os
@@ -10,6 +11,8 @@ cwd = os.getcwd()
 print("Current folder is %s" % (cwd))
 
 mnist = input_data.read_data_sets("data/", one_hot=True)
+#mnist.train.num_examples 
+ 
 
 def print_np(x):
     print('SHAPE OF is %s' % (x.shape,))
@@ -28,9 +31,41 @@ def print_typeshape(img):
 #plt.title("ORIGINAL CAT")
 #plt.draw()
 
-print_np(mnist.train.images)
-print_np(mnist.train.labels)
+#print_np(mnist.train.images)
+#print_np(mnist.train.labels)
 
+
+
+ntrain = mnist.train.images.shape[0] #row 갯수
+nsample = 3 
+randidx = np.random.randint(ntrain, size = nsample)
+
+for i in randidx :
+    imgvec = mnist.train.images[i,:]
+    labelvec = mnist.train.labels[i,:]
+    img = np.reshape(imgvec, (28,28))
+    label = np.argmax(labelvec)
+    plt.matshow(img, cmap=plt.get_cmap('gray'))
+    plt.title("[%d] DATA / LABEL IS [%d]" %(i,label))
+
+
+ntrain = 10
+randindices = np.random.permutation(ntrain)
+print (randindices.shape)
+
+ntrain = 10
+nbatch = 4
+
+niter = ntrain // nbatch + 1
+for i in range(niter):
+    currindices = randindices(i*nbatch:(i+1)*nbatch)
+    print ("iTER : [ %d ] BATCH INDEX : %s " % (i, currindices))
+
+    xbatch = mnist.train.images(currindices, :)
+    ybatch = mnist.train.labels(currindices, :)
+
+    print (" Shape of 'xbatch' is %s" % (xbatch.shape,))
+    print (" Shape of 'ybatch' is %s" % (ybatch.shape,))
 
 
 
