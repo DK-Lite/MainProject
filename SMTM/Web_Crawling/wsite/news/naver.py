14fcd7621e7866d5feb8d530d07b457ebd63c8c4
DK-Lite <kdk5go@gmail.com>
2018-07-08 23:01:01 +0900

    SMTM 1.01 -fix code

--- SMTM/Web_Crawling/wsite/news/naver.py
+++ SMTM/Web_Crawling/wsite/news/naver.py
@@ -1,4 +1,5 @@
 import os, sys, re
+import threading
 
 from bs4 import BeautifulSoup
 import requests
@@ -6,6 +7,45 @@ import requests
 FOLDER_PATH = os.path.dirname(__file__) + '/'
 NAVER_BREAKING_NEWS_URL = 'http://m.news.naver.com/officeMain.nhn'
 
+companys = {
+    'AP연합뉴스',
+    'enews24',
+    'EPA연합뉴스',
+    'KBS 연예',
+    'MBC연예',
+    'MBN',
+    'MK스포츠 ',
+    'OSEN',
+    'TV리포트',
+    'YTN',
+    '경향신문',
+    '노컷뉴스',
+    '뉴스1',
+    '뉴시스',
+    '데일리안',
+    '매일경제',
+    '머니S',
+    '머니투데이',
+    '서울경제',
+    '스타뉴스',
+    '스포츠서울',
+    '스포츠조선',
+    '스포티비뉴스',
+    '엑스포츠뉴스',
+    '여성신문',
+    '연합뉴스',
+    '인터풋볼',
+    '조선일보',
+    '조이뉴스24',
+    '중앙일보',
+    '파이낸셜뉴스',
+    '한국경제',
+    '한국경제TV',
+    '한국일보',
+    '헤럴드POP'
+}
+
+term = 10
 
 # open URL
 def open_Url(URL):
@@ -14,21 +54,27 @@ def open_Url(URL):
     soup = BeautifulSoup(res.content, 'lxml', from_encoding='utf-8')
     return soup
 
-# get text at site
-def get_text(URL) :
+def get_effective_article(html) :
+
+    if html.find_all('div', {'id' : 'dic_area'}):
+        return html.find_all('div', {'id' : 'dic_area'})[0].text
+
+    if html.find_all('div', {'id' : 'contentArea'}):
+        return html.find_all('div', {'id' : 'contentArea'})[0].text
+
+    return ""
+
+# get text in site
+def get_article_parser(URL) :
+
     html = open_Url(URL)
 
-    #get article
-    divs = html.find_all('div', {'id' : 'dic_area'})
+    #get article and time
+    _article = get_effective_article(html)
+    _time = html.find_all('span', {'class' : 'media_end_head_info_datestamp_time'})[0].text
 
-    #write text
-    text = ""
-    for div in divs:
-        text = text + str(div.find_all(text=True)) 
+    return _article, _time
  
-    return text
- 
-
  # clean text 
 def clean_text(text):
     cleaned_text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]','', text)
@@ -36,39 +82,31 @@ def clean_text(text):
     return cleaned_text
 
 # get all headline
-def get_headlines(html):
+def get_headlines(URL):
 
+    html = open_Url(URL)
     divs = html.find_all('ul', {'class' : 'press_newsflash_ct'})
     headlines = divs[0].find_all('li')
 
     list = []
     for headline in headlines:
 
-        #info     = headline.find_all('a')[0]
-
-        #_title   = clean_text(info.text)
-        _title   = headline.find_all('strong', {'class' : 'press_newsflash_headline'})[0].text
-        _title   = clean_text(_title)
-        _url     = headline.find_all('a')[0].attrs['href']
-        _article = get_text('http://m.news.naver.com' + _url)
-        _article = clean_text(_article)
+        _title              = headline.find_all('strong', {'class' : 'press_newsflash_headline'})[0].text
+        _company            = headline.find_all('em', {'class' : 'press_newsflash_info_press'})[0].text
+        _url                = headline.find_all('a')[0].attrs['href']
+        _article, _time     = get_article_parser('http://m.news.naver.com' + _url)
 
         list.append( 
             {
                 'title'     : _title,
                 'url'       : _url,
-                'article'   : _article
+                'article'   : _article,
+                'company'   : _company,
+                'time'      : _time
             }
         )
 
     return list
-
-# refresh 
-def get_refresh_site() :
-
-    naver_html = open_Url(NAVER_BREAKING_NEWS_URL)
-
-    return get_headlines(naver_html)
 
 #file writer
 def file_writer(path, str):
@@ -78,17 +116,28 @@ def file_writer(path, str):
 
 
 # main fuc
-def main():
+def startTimer():
 
-    lists = get_refresh_site()
-    
+    lists = get_headlines(NAVER_BREAKING_NEWS_URL)
+
     for list in lists:
-        file_name   = list['title'] + '.txt'
+
+        # clean text 
+
+
+        file_name   = list['company'] + '.txt'
         file_text   = list['article']
-        file_writer('/'+ file_name, file_text)
+        file_writer("./" + file_name, file_text)
+
+    # timer standard logic
+    timer = threading.Timer(term, startTimer)
+    timer.start()
 
 
-# run 
-main()
+ #main start point
+if __name__ == '__main__':
+    startTimer()
+
+
 
 
